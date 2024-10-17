fb5ae65480e5ec8ee50609aae19cbd55dab242e7
DiegoCanales1845 <81333643+DiegoCanales1845@users.noreply.github.com>
2024-10-17 20:29:17 -0300

    Update CRUD.php

--- CRUD/CRUD.php
+++ CRUD/CRUD.php
@@ -9,21 +9,27 @@ if ($conexion->connect_error) {
 // Insertar o Actualizar Socio
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = $_POST['id'] ?? null;
-    $nombre = $_POST['nombre'];
     $rut = $_POST['rut'];
+    $nombre_completo = $_POST['nombre_completo'];
+    $apellido_completo = $_POST['apellido_completo'];
+    $direccion = $_POST['direccion'];
     $telefono = $_POST['telefono'];
     $correo = $_POST['correo'];
+    $fecha_nacimiento = $_POST['fecha_nacimiento'];
+    $oficio_profecion = $_POST['oficio_profecion'];
 
     if ($id) {
         // Actualizar
-        $sql = "UPDATE socios SET nombre='$nombre', rut='$rut', telefono='$telefono', correo='$correo' WHERE id=$id";
+        $sql = "UPDATE socios SET rut='$rut', nombre_completo='$nombre_completo', apellido_completo='$apellido_completo', direccion='$direccion' telefono='$telefono', 
+                correo='$correo', fecha_nacimiento='$fecha_nacimiento', oficio_profecion='$oficio_profecion' WHERE id=$id";
     } else {
         // Insertar nuevo socio
-        $sql = "INSERT INTO socios (nombre, rut, telefono, correo) VALUES ('$nombre', '$rut', '$telefono', '$correo')";
+        $sql = "INSERT INTO socios (rut, nombre_completo, apellido_completo, direccion, telefono, correo, fecha_nacimiento, oficio_profecion) 
+                VALUES ('$rut', '$nombre_completo', '$apellido_completo', '$direccion', '$telefono', '$correo', '$fecha_nacimiento', '$oficio_profecion')";
     }
 
     if ($conexion->query($sql) === TRUE) {
-        echo "<script>alert('Socio Registrado o Actualizado');</script>";
+        echo "<script>alert('El Socio a sido Registrado o Actualizado');</script>";
     } else {
         echo "Error: " . $sql . "<br>" . $conexion->error;
     }
