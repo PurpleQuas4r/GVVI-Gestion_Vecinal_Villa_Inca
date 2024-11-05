6717747f8c05c91684500a8a5c2229818d8707e5
PurpleQuas4r <98972169+PurpleQuas4r@users.noreply.github.com>
2024-11-04 21:57:37 -0300

    Proyecto con el dashboard-publicaciones
    Se creo la vista y funcionalidad de las publicaciones.
    
    Se inserta en la base de datos todos los datos correspondientes.

---
+++ fase 2/Login/db.php
@@ -0,0 +1,15 @@
+<?php
+// Archivo: db.php
+
+$host = 'localhost';
+$db = 'villapor_GVVI'; // Nombre de la base de datos
+$user = 'villapor'; // Usuario de MySQL
+$password = '7#xSWU9za93;Ww'; // Contrasena de MySQL
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error al conectar a la base de datos: " . $e->getMessage());
+}
+?>
