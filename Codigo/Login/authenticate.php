b5150505dc91d19a0f33278db91c3ecdf2004cee
PurpleQuas4r <miguel.donoso.silvestre@gmail.com>
2024-11-13 00:28:49 -0300

    Codigo actualizado. Uso de git-bash

---
+++ Codigo/Login/authenticate.php
@@ -0,0 +1,51 @@
+<?php
+// Archivo: authenticate.php
+require 'db.php'; // Importamos la conexión a la base de datos
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start(); // Aseguramos que la sesión se inicie al principio del script
+
+// Comprobamos si el formulario fue enviado
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    try {
+        // Preparamos la consulta para buscar el email en la base de datos
+        $stmt = $pdo->prepare('SELECT * FROM usuario WHERE correo_electronico = :email');
+        $stmt->bindParam(':email', $email);
+
+        // Ejecutar la consulta
+        $stmt->execute();
+
+        // Verificamos si se encontró un usuario
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user) {
+            // Si se encontró el usuario, verificamos la contraseña sin hash
+                if ($password === $user['password']) {
+                    $_SESSION['user_id'] = $user['id'];
+                    $_SESSION['logged_in'] = true;
+                    header('Location: /dashboard-Admin.php');
+                    exit();
+                } else {
+                    echo "Contraseña incorrecta.";
+            }
+        } else {
+            // No se encontró el usuario
+            echo "Usuario no encontrado.";
+        }
+    } catch (PDOException $e) {
+        // Capturamos errores de la base de datos y los imprimimos en la consola
+        echo "Error en la base de datos: " . $e->getMessage();
+        die("Error en la base de datos.");
+    }
+} else {
+    // Si no se envió el formulario, redirigimos al formulario de login
+    header('Location: login.php');
+    exit();
+}
+?>
---
+++ Codigo/script.js
@@ -0,0 +1,22 @@
+const menuItems = document.querySelectorAll('#sidebar ul li a.menu-item');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', function(event) {
+        const parent = this.parentElement;
+
+        // Verifica si el enlace debe redirigir (si tiene un submenú o no)
+        if (!this.classList.contains('no-prevent')) {
+            event.preventDefault(); // Prevenir comportamiento predeterminado de enlace solo para ciertos casos
+        }
+
+        // Cerrar todos los otros menús abiertos
+        document.querySelectorAll('#sidebar ul li').forEach(li => {
+            if (li !== parent) {
+                li.classList.remove('active');
+            }
+        });
+
+        // Alternar el submenú actual
+        parent.classList.toggle('active');
+    });
+});
