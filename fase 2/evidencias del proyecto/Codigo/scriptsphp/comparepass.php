76db1f749b0873d86722840dd4078c0dd35f18ee
miguel <miguel.donoso.silvestre@gmail.com>
2024-11-14 18:29:00 -0300

    Archivos Arreglados

---
+++ fase 2/evidencias del proyecto/Codigo/scriptsphp/comparepass.php
@@ -0,0 +1,11 @@
+<?php
+$hash_en_base_datos = 'h$2y$10$194gOUFWzWYl1uc6F.2PK.sZEi58nRysvI9V1G1fp96Ym7Z2qgKXa'; // Copia el hash desde la base de datos
+$password_ingresada = 'userPass';
+
+if (password_verify($password_ingresada, $hash_en_base_datos)) {
+    echo "¡Contraseña verificada!";
+} else {
+    echo "Contraseña incorrecta.";
+}
+
+?>
