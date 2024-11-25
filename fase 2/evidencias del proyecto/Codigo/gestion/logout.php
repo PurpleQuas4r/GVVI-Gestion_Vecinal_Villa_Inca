03e2c51beed1f037a1f1b5b4d6e12d7f34068600
PurpleQuas4r <miguel.donoso.silvestre@gmail.com>
2024-11-25 18:34:41 -0300

    Test subir a git

---
+++ fase 2/evidencias del proyecto/Codigo/gestion/logout.php
@@ -0,0 +1,24 @@
+<?php
+// Archivo: logout.php
+
+session_start(); // Iniciar la sesión
+
+// Destruir todas las variables de sesión
+$_SESSION = array();
+
+// Si se desea destruir la sesión completamente, también se puede eliminar la cookie de la sesión.
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Finalmente, destruir la sesión.
+session_destroy();
+
+// Redirigir a la página principal (index.html)
+header("Location: /Login/login.html");
+exit();
+?>
